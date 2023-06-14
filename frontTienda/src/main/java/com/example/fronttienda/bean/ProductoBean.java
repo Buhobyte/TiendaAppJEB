04f94f067404058389e1f0362f9c83cbb6c95880
Buhobyte <byron.lopez.begins@gmail.com>
2023-06-14 03:40:39 -0500

    Fix faces servelet error

---
+++ frontTienda/src/main/java/com/example/fronttienda/bean/ProductoBean.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.example.fronttienda.bean;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+/**
+ *
+ * @author ka
+ */
+@Named(value = "productoBean")
+@ViewScoped
+public class ProductoBean implements Serializable {
+
+    /**
+     * Creates a new instance of ProductoBean
+     */
+    public ProductoBean() {
+    }
+    
+}
