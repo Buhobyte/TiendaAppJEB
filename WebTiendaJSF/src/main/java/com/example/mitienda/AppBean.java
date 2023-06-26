76a9db1c7aae14b403968f115315295a84508c8d
Buhobyte <byron.lopez.begins@gmail.com>
2023-06-25 22:05:33 -0500

    update

---
+++ WebTiendaJSF/src/main/java/com/example/mitienda/AppBean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.example.mitienda;
+
+import javax.inject.Named;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author ka
+ */
+@Named(value = "appBean")
+@ApplicationScoped
+public class AppBean {
+
+    /**
+     * Creates a new instance of AppBean
+     */
+    public AppBean() {
+    }
+    
+}
